f6fd8877d76f8d0ac53bcd0264da4331aa85e29e
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-07-18 09:48:37 +0800

    Initial Commit

---
+++ MacOSDockAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacOSDockAnimation
+//
+//  Created by Runhua Huang on 2022/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacOSDockAnimation/MacOSDockAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSDockAnimationApp.swift
+//  MacOSDockAnimation
+//
+//  Created by Runhua Huang on 2022/7/18.
+//
+
+import SwiftUI
+
+@main
+struct MacOSDockAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
